5abdc693007a920fe481950a02d08eb796274d16
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-10-12 13:15:47 +0530

    first commit

---
+++ sample/java.java
@@ -0,0 +1,10 @@
+/**
+ * java
+ */
+public class java {
+
+        public static void main(String[] args) {
+                // write your code here
+                System.out.println("heloo world");
+        }
+}
