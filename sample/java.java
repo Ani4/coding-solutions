c6a37e84e692ed2777e785e61969cb8111175da8
Aniket kumar sharma <sharma.ani.4298@gmail.com>
2020-10-16 15:20:14 +0530

    spiral traversing

--- sample/java.java
+++ sample/java.java
@@ -1,13 +1,25 @@
 import java.util.*;
 
+import java.io.*;
+
 /**
  * java
  */
+
 public class java {
         static Scanner scan = new Scanner(System.in);
 
-        public static void main(String[] args) {
+        public static void main(String[] args) throws Exception {
                 // write your code here
-                System.out.println("heloo world");
+                BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+
+                // PrintWriter class prints formatted representations
+                // of objects to a text-output stream.
+                PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+
+                // Your code goes Here
+
+                br.readLine(pw.write());
+                pw.flush();
         }
 }
